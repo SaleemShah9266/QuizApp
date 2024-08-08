ab82e7e63579c3049dba898e70a998a66d21bca5
Saleem Shah <saluu1212@gmail.com>
2024-08-08 22:13:06 +0500

    Changes to be committed: 	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quizapp/HomePage.dart';
 
 void main() {
-  runApp(const MyApp());
-  
+  runApp(const MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
